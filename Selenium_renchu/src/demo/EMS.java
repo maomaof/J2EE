6424e3998030afde242a5a4bcb084746e63c97c0
xiyfeng <xiyu.feng@thalesgroup.com>
2020-04-25 16:10:51 +0800

    remove file that should not add

--- Selenium_renchu/src/demo/EMS.java
+++
@@ -1,7 +0,0 @@
-package demo;
-
-public class EMS {
-	public String addABC(String input) {
-		return new String(input + " ABC");
-	}
-}
--- Selenium_renchu/src/renchuTool/RenchuTool.java
+++
@@ -1,7 +0,0 @@
-package renchuTool;
-
-public class RenchuTool {
-	protected static void pop(Object obj) {
-		System.out.println(obj);
-	}
-}
