564c651015e30ed6475e475ef91b8d70b5da4437
Jeremy Jia <jjia@jjia-macbookpro.opentext.net>
2020-06-26 23:23:04 +0800

    fix an script issue

--- Spring/script/jpg2video.sh
+++ Spring/script/jpg2video.sh
@@ -16,6 +16,9 @@ if [ -z $MY_WIDTH ] ; then
     echo "The usage is:" $MY_NAME "360" "1"
 else
     echo "Call ffmpeg to generate video"
+    if [ ! -f $FFMPEGDIR/ffmpeg ];then
+       FFMPEGDIR=/usr/local/bin
+     fi
     $FFMPEGDIR/ffmpeg -framerate $RATE -i $THISDIR/%d.jpg -r $RATE -s $MY_WIDTH"x"$MY_WIDTH -y vSubtitle.mp4
 fi
 echo "-------------------------------end----------------------------------------"
