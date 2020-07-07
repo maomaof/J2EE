216fdc92bee3afce7cf3df0f0eec231ae9423766
yongling <yongling.huang@group6.io>
2020-07-07 00:09:45 +0000

    plx12.js v0.52

--- Spring/script/plx12.js
+++ Spring/script/plx12.js
@@ -1,4 +1,4 @@
-﻿const tag = "[plx12.js_v0.41]";
+﻿const tag = "[plx12.js_v0.52]";
 var v1 = bl$("id_div_4_Plx1_v1");
 v1.innerHTML = tag+new Date;
 //*
@@ -27,14 +27,18 @@ function classFrame(){
     }
   } 
   function sprite(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
+    this.text = "spriteText.";
     this.x = x;
     this.y = y;    
+    this.size = 50;
+    this.color= "222,123,50";
+
+    var _width = width;
+    var _height = height;
     this.update = function(){ 
         ctx = myGameArea.context;
         ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillRect(this.x, this.y, _width, _height);
     }
   }
   function updateGameArea() {
@@ -48,6 +52,7 @@ function classFrame(){
   this.initGame = function () { 
     var tb = blo0.blDiv(v1, v1.id + "tb","tb",blGrey[1]);
     var b1 = blo0.blBtn(tb,tb.id + "b1","b1",blGrey[2]);
+    var b2 = blo0.blBtn(tb,tb.id + "b2","b2",blGrey[2]);
     var d1 = blo0.blDiv(v1, v1.id + "d1","d1",blGrey[3]);
     b1.onclick = function(){
       var d = new Date;
@@ -56,6 +61,30 @@ function classFrame(){
       var y = d.getMilliseconds()%100;
       var sp = new sprite(20, 20, "green", x, y);
       spList.push(sp);
+    }
+    b2.onclick = function(){
+      var s = "2";
+      var o = {};
+      var r = {};
+      var fs = [];
+      o.request = r;
+      r.version = "v0. 14";
+      r.width   = 480;
+      r.height  = 270;
+      r.music   = "1.mp3";
+      r.rate    = 1;
+      r.frames  = fs; 
+
+      var f={};
+      f.number = 1;
+      f.time = 60;
+      f.objects = spList;
+      fs.push(f);
+
+      f.backgroundColor = "123,45,200";
+      
+      s =   JSON.stringify(o);
+      bl$("id_ta_4_script_editor").value = s;
     }
   };
 
