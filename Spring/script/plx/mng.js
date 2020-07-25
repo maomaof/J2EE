fbdfad5c2102b296e26e63176a904334b888e80d
littleflute <13699175164@sina.cn>
2020-07-25 03:14:53 +0000

    mng.js v0.14

--- Spring/script/plx/mng.js
+++ Spring/script/plx/mng.js
@@ -1,3 +1,3 @@
-﻿const tag = "[plx/mng.js_v0.13]";
-var d = blo0.blMD("id_mdiv_load_plxMng", tag,  300,100,400,300, blGrey[5]);
+﻿const tag = "[plx/mng.js_v0.14]";
+var d = blo0.blMD("id_mdiv_load_plxMng", tag,  300,100,400,100, blGrey[5]);
 _on_off_div(null,d);
