ddc81a1bc7ecf7f3459479a5db6f1609a955f0c9
unknown <yongling.huang@group6.io>
2020-08-16 01:48:48 -0700

    p1.js v0.214

--- Spring/script/plx/p1.js
+++ Spring/script/plx/p1.js
@@ -1,16 +1,26 @@
 ﻿
-const p1Tag = "[plx/p1.js_v0.154]";
+const p1Tag = "[plx/p1.js_v0.214]";
+
 const btn4p1 = bl$("plx_p1_btn");
+
 if(btn4p1){ 
     btn4p1.v = blo0.blMD(btn4p1.id+p1Tag,p1Tag,210,88,555,150,blGrey[0]);
     var tb = blo0.blDiv(btn4p1.v,btn4p1.v.id+"tb","tb",blGrey[1]);
-    tb.btnStoryBoard = blo0.blBtn(tb,tb.id+"btnStoryBoard","storyBoard",blGrey[2]);
+    tb.btnStoryBoard = blo0.blBtn(tb,"btnStoryBoard","storyBoard",blGrey[2]);
     tb.btnStoryBoard.style.float = "left";
+    
     tb.btnStoryBoard.onclick = function(){
        if(!this.sb)  this.sb = new CStoryBoard(btn4p1.v);
        this.sb.show(this);
     }
     
+    tb.btnPlayground = blo0.blBtn(tb,"btnPlayground","Playground",blGrey[2]);
+    tb.btnPlayground.style.float = "left";
+    
+    tb.btnPlayground.onclick = function(){
+        if(!this.pg)  this.pg = new CPlayground(btn4p1.v);
+        this.pg.show(this);
+    }
     btn4p1.onclick = function(){
         var b = this;
         _on_off_div(b,b.v);
@@ -19,31 +29,45 @@ if(btn4p1){
     btn4p1.onclick();
 }
  
+function CPlayground(parentDiv){
+    var p = parentDiv;
+    var ui = null;
+    var w = 360;
+    var h = 240;
+    
+
+    this.show = function(b){
+        if(!ui){
+            ui=blo0.blMDiv(p,"id_mdiv_4_playground","playground",555,5,w,111,blGrey[0]);
+            var tb = blo0.blDiv(ui, "id_4_tb_playground","tb",blGrey[1]);
+            var v1 = blo0.blDiv(ui,ui.id+"v1","",blGrey[1]);            
+            var cvs = document.createElement("canvas");
+            cvs.width = w;
+            cvs.height = h;
+            cvs.style.backgroundColor = "grey";
+            cvs.style.float = "left";
+
+            v1.appendChild(cvs);
+            
+            cvs.addEventListener('mousedown', function (e) {
+                var x = e.offsetX;
+                var y = e.offsetY;
+                o.mousedown(cvs.getContext("2d"),x,y);                
+            });
+            
+            
+            var itv = setInterval(o.ftnTimer, 20,cvs.getContext("2d"),w,h);
+        }
+        _on_off_div(b,ui);
+        b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];   
+    }; 
+}
 function CStoryBoard(parentDiv){
 
     var v = "CStoryBoard v0.12";
     var ui = null;
     var p = parentDiv; 
-    var o = {};
-    o.uiCards = function(_p,_c){  
-        _p.style.overflow = "auto";        
-        var cardV = blo0.blDiv(_p,_p.id+"cardV","cardV",blGrey[2]);
-        cardV.style.width = 20*111 +"px";
-        cardV.style.height = "50px";
-        cardV.style.backgroundColor = "coral";
-        cardV.style.float = "left";
-    };
-    o.uiColum = function(ui){           
-        var s = '<div class="w3-col s3 w3-green w3-center"><p>s6</p></div>';
-        s+='<div class="w3-col s9 w3-dark-grey w3-center" id="uiRight">   </div>';
-        var v1 = blo0.blDiv(ui,ui.id+"v1",s,blGrey[2]);
-       
-        o.addClass(v1,"w3-row"); 
-        var r = bl$("uiRight");  
-        o.uiCards(r,"red");  
-    };
-    o.addClass = function (d,cn) {        d.classList.add(cn);    };
-      
+    
      
     this.show = function(b){
         if(!ui){
@@ -118,3 +142,55 @@ function CClient(){
         blo0.blAjx(w, "http://localhost:8080/getResourceOnServer?filetype=mp3" ); 
     };
 }
+
+var o = {};
+o.x = 50;
+o.y = 30;
+o.s = "o.s";
+o.mousedown = function(ctx,x,y){
+    o.s = x + ":" + y;
+    o.x = x;
+    o.y = y;    
+};
+o.ftnTimer = function(ctx,w,h){ 
+    ctx.clearRect(0, 0, w, h);
+    
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0,0,w,h);
+ 
+    o.text(ctx,"xd--" + Date(),15,20);  
+    o.text(ctx,o.s,o.x,o.y);  
+};
+o.text = function(ctx,txt,x,y){ 
+    ctx.font= 12 + "px Comic Sans MS";
+    ctx.fillStyle = "white";
+    ctx.fillText(txt, x,y); 
+};
+    o.uiCards = function(_p,_c){  
+        _p.style.overflow = "auto";        
+        var cardV = blo0.blDiv(_p,_p.id+"cardV","cardV",blGrey[2]);
+        cardV.style.width = 20*111 +"px";
+        cardV.style.height = "50px";
+        cardV.style.backgroundColor = "coral";
+        cardV.style.float = "left";
+    };
+    o.uiColum = function(ui){           
+        var s = '<div class="w3-col s3 w3-green w3-center"><p>s6</p></div>';
+        s+='<div class="w3-col s9 w3-dark-grey w3-center" id="uiRight">   </div>';
+        var v1 = blo0.blDiv(ui,ui.id+"v1",s,blGrey[2]);
+       
+        o.addClass(v1,"w3-row"); 
+        var r = bl$("uiRight");  
+        o.uiCards(r,"red");  
+    };
+    o.addClass = function (d,cn) {        d.classList.add(cn);    };
+      
+
+    var b = bl$("btnStoryBoard");    
+    o.addClass(b,"w3-button"); 
+    o.addClass(b,"w3-blue"); 
+ 
+
+    var b = bl$("btnPlayground");    
+    o.addClass(b,"w3-button"); 
+    o.addClass(b,"w3-green"); 
