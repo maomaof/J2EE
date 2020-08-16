e7375e83ddba38a0a5912a944f57118056c6fa80
unknown <yongling.huang@group6.io>
2020-08-15 18:07:31 -0700

    2.html plx/p1.js*

--- Spring/script/plx/p1.js
+++ Spring/script/plx/p1.js
@@ -1,8 +1,16 @@
 ﻿
-const p1Tag = "[plx/p1.js_v0.153]";
+const p1Tag = "[plx/p1.js_v0.154]";
 const btn4p1 = bl$("plx_p1_btn");
 if(btn4p1){ 
-    btn4p1.v = blo0.blMD(btn4p1.id+p1Tag,p1Tag,210,88,155,150,blGrey[0]);
+    btn4p1.v = blo0.blMD(btn4p1.id+p1Tag,p1Tag,210,88,555,150,blGrey[0]);
+    var tb = blo0.blDiv(btn4p1.v,btn4p1.v.id+"tb","tb",blGrey[1]);
+    tb.btnStoryBoard = blo0.blBtn(tb,tb.id+"btnStoryBoard","storyBoard",blGrey[2]);
+    tb.btnStoryBoard.style.float = "left";
+    tb.btnStoryBoard.onclick = function(){
+       if(!this.sb)  this.sb = new CStoryBoard(btn4p1.v);
+       this.sb.show(this);
+    }
+    
     btn4p1.onclick = function(){
         var b = this;
         _on_off_div(b,b.v);
@@ -10,6 +18,45 @@ if(btn4p1){
     } 
     btn4p1.onclick();
 }
+ 
+function CStoryBoard(parentDiv){
+
+    var v = "CStoryBoard v0.12";
+    var ui = null;
+    var p = parentDiv; 
+    var o = {};
+    o.uiCards = function(_p,_c){  
+        _p.style.overflow = "auto";        
+        var cardV = blo0.blDiv(_p,_p.id+"cardV","cardV",blGrey[2]);
+        cardV.style.width = 20*111 +"px";
+        cardV.style.height = "50px";
+        cardV.style.backgroundColor = "coral";
+        cardV.style.float = "left";
+    };
+    o.uiColum = function(ui){           
+        var s = '<div class="w3-col s6 w3-green w3-center"><p>s6</p></div>';
+        s+='<div class="w3-col s6 w3-dark-grey w3-center" id="uiRight"> <p>s6</p> </div>';
+        var v1 = blo0.blDiv(ui,ui.id+"v1",s,blGrey[2]);
+       
+        o.addClass(v1,"w3-row"); 
+        var r = bl$("uiRight");  
+        o.uiCards(r,"red");  
+    };
+    o.addClass = function (d,cn) {        d.classList.add(cn);    };
+      
+     
+    this.show = function(b){
+        if(!ui){
+            ui=blo0.blDiv(p,p.id+"_StoryBoard",v,blGrey[1]);   
+            o.addClass(ui,"w3-row");  
+            o.addClass(ui,"w3-red");
+
+            o.uiColum(ui);   
+        }
+        _on_off_div(b,ui);
+        b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];   
+    }; 
+} 
 
 function CClient(){
     var w = {};
